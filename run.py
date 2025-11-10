66d3dd4a322b7b3ae0649ffdde0af6704f97c4e6
Santiago Pinilla <spinillac@unal.edu.co>
2025-11-10 15:56:08 +0100

    Cambio radio y número de particulas

--- run.py
+++ run.py
@@ -3,12 +3,12 @@ from animate import Animate
 import numpy as np
 
 def main():
-    np.random.seed(1)  # set random numbers to be always the same
-    simulation = Simul(simul_time=0.006, sigma=1., L=7)  # sigma particle radius # L box size
+    np.random.seed()  # set random numbers to be always the same
+    simulation = Simul(simul_time=0.006, sigma=0.1, L=7)  # sigma particle radius # L box size
     print(simulation.__doc__)  # print the documentation from the class
 
     animate = Animate(simulation)
-    animate.go(nframes=200)  # number of animation steps
+    animate.go(nframes=400)  # number of animation steps
     print(simulation)  # print last configuration to screen
 
 
