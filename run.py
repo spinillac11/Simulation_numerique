b2a8fe41c4a6129ce72015cb6629475d0ba232a2
Santiago Pinilla <spinillac@unal.edu.co>
2025-11-30 12:01:27 +0100

    P vs N working

--- run.py
+++ run.py
@@ -1,37 +1,90 @@
-from simul import Simul
-from animate import Animate
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from simul import Simul
+
+# -------------------------------
+# Utility functions
+# -------------------------------
+
+def pressure_vs_N(L, sigma_min, sigma_max, simul_time, Ns, steps, K_scale):
+    Pressures = []
+    Energies = []
+    bs = []
+
+    for N in Ns:
+        sim = Simul(simul_time=simul_time, sigma_min=sigma_min, sigma_max=sigma_max, L=L, N=N, K_scale=K_scale)
+
+        b = np.pi * np.mean(sim.sigma ** 2)
+        bs.append(b)
+
+        P_accum = 0
+        E_accum = 0
+
+        for _ in range(steps):
+            p, k = sim.md_step()
+            P_accum += p
+            E_accum += k
+
+        Pressures.append(P_accum / steps)
+        Energies.append(E_accum / steps)
+
+    return np.array(Pressures), np.array(Energies), np.array(bs)
+
+
+def plot_ideal_gas(P, Ec, V, Ns):
+    ratio = P * V / Ec
+    plt.figure()
+    plt.plot(Ns, ratio, marker='o')
+    plt.xlabel("Number of particles N")
+    plt.ylabel("PV / Ec")
+    plt.title("Ideal Gas Test: PV/Ec = constant?")
+    plt.grid()
+    plt.show()
+
+
+def plot_van_der_waals(P, Ec, V, Ns, b):
+    Ns = np.array(Ns)       
+    b = np.array(b)
+    ratio = P * (V - Ns * b) / Ec
+    plt.figure()
+    plt.plot(Ns, ratio, marker='o')
+    plt.xlabel("Number of particles N")
+    plt.ylabel("P (V/N - b) / Ec")
+    plt.title("Van der Waals Test: P(V/N-b)/Ec = constant?")
+    plt.grid()
+    plt.show()
 
 
 def main():
-    P_list=[]
-    K_list=[]
-    np.random.seed()  # set random numbers to be always the same
-    simulation = Simul(simul_time=0.006, sigma_min=0.1, sigma_max=0.2, L=10, N=100) 
-    print(simulation.__doc__)  # print the documentation from the class
+    print("Select a simulation to run:")
+    print("1) Ideal Gas Test (very small particles)")
+    print("2) Van der Waals Test (large particles)")
+
+    choice = input("Enter 1, or 2: ")
+    #choice2 = input("Enter 1, 2, or 3:")
+
+    L = 10
+    simul_time = 0.5
+    Ns = [4, 8, 10, 12, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160]
+    steps = 200
+    K_scale = 2
     
-    pressure = 0
-    kinetic = 0
-    for i in range(5000):
-        p, k=simulation.md_step()
-        pressure += p
-        kinetic += k
-        if i%100 == 0:
-            P_list.append(pressure/(100*0.006))
-            K_list.append(kinetic/100)
-            pressure = 0 
-            kinetic = 0
-    
-    #print(P_list)
+    if choice == "1":
+        print("\nRunning Ideal Gas Test...")
+        P, Ec, b = pressure_vs_N(L=L, sigma_min=0.01, sigma_max=0.01, simul_time=simul_time, Ns=Ns, steps=steps, K_scale=K_scale)
+        V = L * L
+        plot_ideal_gas(P, Ec, V, Ns)
 
-    plt.plot(P_list)
-    plt.show()
+    elif choice == "2":
+        print("\nRunning Van der Waals Test...")
+        P, Ec, b = pressure_vs_N(L=L, sigma_min=0.2, sigma_max=0.2, simul_time=simul_time, Ns=Ns, steps=steps, K_scale=K_scale)
+        V = L * L
+        plot_van_der_waals(P, Ec, V, Ns, b)
 
-    animate = Animate(simulation)
-    animate.go(nframes=400)  # number of animation steps
-    #print(simulation)  # print last configuration to screen
+    else:
+        print("Invalid option.")
+        return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- simul.py
+++ simul.py
@@ -7,7 +7,7 @@ class Simul:
     This is the prototype of the simulation code
     It moves the particles with at _velocity, using a vector notation: numpy should be used.
     """
-    def __init__(self, simul_time, sigma_min, sigma_max, L, N):
+    def __init__(self, simul_time, sigma_min, sigma_max, L, N, K_scale):
         np.seterr(all='ignore')  # remove errors in where statements
         self.N = N   
         self.sigma = self.sigma = np.random.uniform(sigma_min, sigma_max, size=self.N)  # particles random radius
@@ -18,12 +18,12 @@ class Simul:
         self.position[:,0] = self.position[:,0]*(self.L - 2*self.sigma) + self.sigma # starting x positions
         self.position[:,1] = self.position[:,1]*(self.L - 2*self.sigma) + self.sigma # starting y positions
 
-        self.velocity = 2*np.random.normal(size=self.position.shape)  # random velocities
+        self.velocity = K_scale*np.random.normal(size=self.position.shape)  # random velocities
         self.l, self.m = np.triu_indices(self.position.shape[0], k=1)  # all pairs of indices between particles
 
-        delta_r = self.position[self.m] - self.position[self.l]
-        abs_r = np.sqrt(np.sum(delta_r * delta_r, axis = 1))
-        count = 0
+        # delta_r = self.position[self.m] - self.position[self.l]
+        # abs_r = np.sqrt(np.sum(delta_r * delta_r, axis = 1))
+        # count = 0
 
         # while np.min(abs_r) < 2*self.sigma:
         #     self.position = (self.L-2*self.sigma)*np.random.rand(self.N, 2) + self.sigma
@@ -33,8 +33,6 @@ class Simul:
         #     if count > 100000:
         #         raise ValueError("To many particles for this sigma")
 
-    #def init_pos(self):
-        
     def wall_time(self):
         positive_time = (self.L-self.sigma[:, None]-self.position)/self.velocity
         neg_time = (self.sigma[:, None]-self.position)/self.velocity
@@ -46,7 +44,6 @@ class Simul:
         particle = where[0][0] #First index par
         direction = where[1][0] #Second index dir
         return first_collision_time, particle, direction
-        # calculate time of first collision, particle involved and direction
 
     def pair_time(self):
         delta_r = self.position[self.m] - self.position[self.l]   # relative positions
@@ -70,10 +67,9 @@ class Simul:
         return first_collision_time, self.l[idx], self.m[idx]
     
     def md_step(self):
-        #print('Simul::md_step')
         ke_start = (self.velocity**2).sum()/2.   # starting kinetic energy
 
-        pressure = 0
+        Delta_P = 0
         current_time = 0
         
         w_time, particle, direction = self.wall_time()
@@ -90,7 +86,7 @@ class Simul:
                 w_time, particle, direction = self.wall_time()
                 p_time, particle_1, particle_2 = self.pair_time()
                 time_min = min(w_time, p_time)
-                pressure += 2*np.abs(self.velocity[particle, direction])
+                Delta_P += 2*np.abs(self.velocity[particle, direction])
     
             else:
                 self.position += p_time * self.velocity
@@ -104,12 +100,13 @@ class Simul:
                 w_time, particle, direction = self.wall_time()
                 time_min = min(w_time, p_time)
 
-        # adapt the position update  as a function of your logic
+        
         self.position += (self.simul_time-current_time) * self.velocity
 
         assert math.isclose(ke_start,  (self.velocity**2).sum()/2.)  # check that we conserve energy after all the collisions
 
-        return pressure/(4*self.L*self.simul_time), ke_start
+        pressure = Delta_P/(4*self.L*self.simul_time)
+        return pressure, ke_start
 
     def __str__(self):   # this is used to print the position and velocity of the particles
         p = np.array2string(self.position)
